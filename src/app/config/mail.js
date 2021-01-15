96986e78bb4b28a2bc731807b6055f2efbf50f3e
guimariz <guimariz@gmail.com>
2021-01-15 04:04:28 -0300

    integração com Mailtrap configurada

--- src/app/config/mail.js
+++ src/app/config/mail.js
@@ -1,8 +1,8 @@
 export default {
-  host: prcess.env.MAIL_HOST,
-  port: prcess.env.MAIL_PORT,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   auth: {
-    user: prcess.env.MAIL_USER,
-    pass: prcess.env.MAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 };
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 import passwordGenerator from 'password-generator';
+import Mail from '../lib/Mail';
 
 export default {
   async store(req, res) {
@@ -9,6 +10,13 @@ export default {
       email,
       password: passwordGenerator(15, false),
     };
+
+    await Mail.sendMail({
+      from: 'DIO <contato@dio.com.br>',
+      to: `${name} <${email}>`,
+      subject: 'Cadastro de usuário',
+      html: `Olá, ${name}, bem vindo a DIO.`,
+    });
 
     return res.json(user);
   },
